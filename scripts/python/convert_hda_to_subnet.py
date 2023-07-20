eef5a8d22e96e87a416fe31ea08bf224d0c80943
zhanghaonan <zhanghaonan@blackjack-inc.com>
2023-07-20 14:50:21 +0800

    complete hda convert_hda_to_subnet

--- scripts/python/convert_hda_to_subnet.py
+++ scripts/python/convert_hda_to_subnet.py
@@ -1,17 +1,6 @@
 import os
 import hou
 
-def CopyParmsFromFolder(folderSet, parent):
-    for parmTemplate in folderSet.parmTemplates():
-        if isinstance(parmTemplate, hou.FolderSetParmTemplate):
-            newFolder = hou.FolderParmTemplate(parmTemplate.name(), parmTemplate.label())
-            newFolder.setFolderType(parmTemplate.folderType())
-            CopyParmsFromFolder(parmTemplate, newFolder)
-            parent.addParmTemplate(newFolder)
-        else:
-            newParm = parmTemplate.clone()
-            parent.addParmTemplate(newParm)
-
 def Convert(selection):
     nodePos = selection.position()
     nodeName = selection.name()
@@ -20,6 +9,10 @@ def Convert(selection):
     newNode = obj.createNode("subnet", nodeName + "_subnet")
     newNode.setPosition(nodePos)
 
+    # copy items
+    newNode.copyItems(selection.allItems())
+
+    # wire input/output nodes
     nodeInputConnectors = selection.inputConnectors()
     for i in range(len(nodeInputConnectors)):
         connector = nodeInputConnectors[i]
@@ -27,35 +20,15 @@ def Convert(selection):
             newNode.setInput(i, connector[j].inputNode(), j)
     
     nodeOutputConnectors = selection.outputConnectors()
-    # print(nodeOutputConnectors)
     for i in range(len(nodeOutputConnectors)):
-        if i > 0:
-            newNode.createNode("output")
         connector = nodeOutputConnectors[i]
         for j in range(len(connector)):
             outputNode = connector[j].outputNode()
-            # outputNode.setInput(connector[j].outputIndex(), newNode, i)
+            outputNode.setInput(connector[j].inputIndex(), newNode, i)
     
-    group = newNode.parmTemplateGroup()
-    baseFolder = hou.FolderParmTemplate("parmsFromHDA", "Parms From HDA")
-    for parm in selection.parms():
-        if isinstance(parm.parmTemplate(), hou.FolderSetParmTemplate):
-            CopyParmsFromFolder(parm.parmTemplate(), baseFolder)
-    group.append(baseFolder)
+    # copy parameters
+    group = selection.parmTemplateGroup()
     newNode.setParmTemplateGroup(group)
 
-    #     tmp = parm.tuple().parmTemplate()
-    #     print(tmp)
-    #     if parm.componentIndex() == 0:
-    #         newParm = tmp.clone()
-    #         newNode.addSpareParmTuple(newParm)
-    #         group.append(newParm)
-    # newNode.setParmTemplateGroup(group)
-
-    # for parm in selection.parms():
-    #     newParm = newNode.parm('parm')
-    #     parm.copyToParmClipboard()
-    #     clip = hou.parmClipboardContents()[0]
-    #     path = clip['path']
-    #     newParm.set(path)
+    selection.destroy()
 
